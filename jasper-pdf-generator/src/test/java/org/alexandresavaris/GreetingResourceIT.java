3b2279c34bce5c540b947fc656fa631d1b7107b5
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-02-09 17:25:48 -0300

    Project structure from Quarkus START CODING.

---
+++ jasper-pdf-generator/src/test/java/org/alexandresavaris/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.alexandresavaris;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
